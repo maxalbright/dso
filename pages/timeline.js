23d88fcba7d1fcdea4875437af8b69d0b181f027
Alice Dao <alicedao@usc.edu>
2023-11-13 22:43:33 -0800

    completed timeline page

--- pages/timeline.js
+++ pages/timeline.js
@@ -24,7 +24,7 @@ const Resume = () => {
       {process.env.NODE_ENV === "development" && (
         <div className="fixed bottom-6 right-6">
           <Button onClick={() => router.push("/edit")} type={"primary"}>
-            Edit Resume
+            Edit Timeline
           </Button>
         </div>
       )}
@@ -49,7 +49,7 @@ const Resume = () => {
                 <Socials />
               </div>
               <div className="mt-5">
-                <h1 className="text-2xl font-bold">Experience</h1>
+                <h1 className="text-2xl font-bold">Birth</h1>
 
                 {resume.experiences.map(
                   ({ id, dates, type, position, bullets }) => (
@@ -64,18 +64,73 @@ const Resume = () => {
                 )}
               </div>
               <div className="mt-5">
-                <h1 className="text-2xl font-bold">Education</h1>
+                <h1 className="text-2xl font-bold">Hogwarts Years</h1>
                 <div className="mt-2">
                   <h2 className="text-lg">{resume.education.universityName}</h2>
                   <h3 className="text-sm opacity-75">
                     {resume.education.universityDate}
                   </h3>
                   <p className="text-sm mt-2 opacity-50">
-                    {resume.education.universityPara}
+                      {resume.education.universityPara.split('\n').map((point, index) => (
+                          <span key={index}>{point}
+                              <br />
+                          </span>
+                      ))}
                   </p>
                 </div>
-              </div>
 
+              </div>
+                <div className="mt-5">
+                    <h1 className="text-2xl font-bold">Post-Grad</h1>
+                    <div className="mt-2">
+                        <h2 className="text-lg">{resume.joiningoftp.orgName}</h2>
+                        <h3 className="text-sm opacity-75">
+                            {resume.joiningoftp.joiningDate}
+                        </h3>
+                        <p className="text-sm mt-2 opacity-50">
+                            {resume.joiningoftp.para.split('\n').map((point, index) => (
+                                <span key={index}>{point}
+                                    <br />
+                          </span>
+                            ))}
+                        </p>
+                    </div>
+
+                </div>
+                <div className="mt-5">
+                    <h1 className="text-2xl font-bold">The First Wizarding War</h1>
+                    <div className="mt-2">
+                        <h2 className="text-lg">{resume.war.orgName}</h2>
+                        <h3 className="text-sm opacity-75">
+                            {resume.war.joiningDate}
+                        </h3>
+                        <p className="text-sm mt-2 opacity-50">
+                            {resume.war.para.split('\n').map((point, index) => (
+                                <span key={index}>{point}
+                                    <br />
+                          </span>
+                            ))}
+                        </p>
+                    </div>
+
+                </div>
+                <div className="mt-5">
+                    <h1 className="text-2xl font-bold">Death</h1>
+                    <div className="mt-2">
+                        <h2 className="text-lg">{resume.death.orgName}</h2>
+                        <h3 className="text-sm opacity-75">
+                            {resume.death.joiningDate}
+                        </h3>
+                        <p className="text-sm mt-2 opacity-50">
+                            {resume.death.para.split('\n').map((point, index) => (
+                                <span key={index}>{point}
+                                    <br />
+                          </span>
+                            ))}
+                        </p>
+                    </div>
+
+                </div>
             </div>
           </div>
         )}
